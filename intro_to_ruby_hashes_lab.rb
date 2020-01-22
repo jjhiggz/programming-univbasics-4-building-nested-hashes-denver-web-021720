c76d204778a42a28eba7784dc6601ff788bf069b
Jonathan Higger <jonathan.higger@gmail.com>
2020-01-22 09:18:39 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,11 +1,12 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-	countries_and_capitals_of_the_world = {'railroads': {peices: {}}
+	countries_and_capitals_of_the_world = {'railroads': {}
 }
 end
 
 def monopoly_with_second_tier
+		countries_and_capitals_of_the_world = {'railroads': {peices: {}}
   # When you start writing the implementation for this method, copy the Hash
   # you built in the previous method. Run the tests. They will fail, but
   # they'll guide you in how to modify what you just did to have more
